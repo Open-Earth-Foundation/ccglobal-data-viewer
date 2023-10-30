f19281fe5a44a97898b255993b531bd1e94dfdcb
Luke Gloege <lukegloege@gmail.com>
2023-10-30 11:34:24 -0400

    feat: convert into multipage app

---
+++ Homepage.py
@@ -0,0 +1,8 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Multipage App'
+)
+
+st.title('Data Coverage Viewer')
+st.write("""Navigate to different pages in the sidebar to understand coverage by city or region""")
